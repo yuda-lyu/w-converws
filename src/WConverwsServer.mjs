c6d1bc4fd5f1bfa666bcbd2f40ebb636fb1724d8
yuda-lyu <firsemisphere@gmail.com>
2021-10-05 21:12:00 +0800

    modify: update pks

--- src/WConverwsServer.mjs
+++ src/WConverwsServer.mjs
@@ -1,9 +1,9 @@
-import WebSocket from 'ws'
+import WebSocket, { WebSocketServer } from 'ws'
 import events from 'events'
 import get from 'lodash/get'
-import genPm from 'wsemi/src/genPm.mjs'
-import urlParse from 'wsemi/src/urlParse.mjs'
-import isfun from 'wsemi/src/isfun.mjs'
+// import genPm from 'wsemi/src/genPm.mjs'
+// import urlParse from 'wsemi/src/urlParse.mjs'
+// import isfun from 'wsemi/src/isfun.mjs'
 import sendSplitData from './sendSplitData.mjs'
 import mergeSplitData from './mergeSplitData.mjs'
 
@@ -91,13 +91,28 @@ function WConverwsServer(opt = {}) {
     }
 
 
-    //WebSocketServer
-    let WebSocketServer = WebSocket.Server
-
-
     //serverSettings
     let serverSettings = {
         port: opt.port,
+        perMessageDeflate: {
+            zlibDeflateOptions: {
+                // See zlib defaults.
+                chunkSize: 1024,
+                memLevel: 7,
+                level: 3
+            },
+            zlibInflateOptions: {
+                chunkSize: 10 * 1024
+            },
+            // Other options settable:
+            clientNoContextTakeover: true, // Defaults to negotiated value.
+            serverNoContextTakeover: true, // Defaults to negotiated value.
+            serverMaxWindowBits: 10, // Defaults to negotiated value.
+            // Below options specified as default values.
+            concurrencyLimit: 10, // Limits zlib concurrency for perf.
+            threshold: 1024 // Size (in bytes) below which messages
+            // should not be compressed if context takeover is disabled.
+        },
     }
 
 
